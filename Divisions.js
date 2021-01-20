939659987008fe6ee814bfa9d82c091b8d7cc41c
killer9238 <75729746+killer9238@users.noreply.github.com>
2021-01-20 18:42:45 +0530

    Add files via upload

---
+++ Divisions.js
@@ -0,0 +1,18 @@
+class Divisions {
+    constructor(x, y, widht, height) {
+        var options = {
+
+            isStatic: true
+        }
+        this.body = Bodies.rectangle(x, y, widht, height, options);
+        this.widht = widht;
+        this.height = height;
+        World.add(world, this.body);
+    }
+    display() {
+        var pos = this.body.position;
+        rectMode(CENTER);
+        fill("white");
+        rect(pos.x, pos.y, this.widht, this.height);
+    }
+};
---
+++ Ground.js
@@ -0,0 +1,17 @@
+class Ground{
+    constructor(x,y,widht,height) {
+      var options = {
+          isStatic: true
+      }
+      this.body = Bodies.rectangle(x,y,widht,height,options);
+      this.widht= widht; 
+      this.height = height;
+      World.add(world, this.body);
+    }
+    display(){
+      var pos =this.body.position;
+      rectMode(CENTER);
+      fill("white");
+    rect(pos.x, pos.y, this.widht,this.height);
+    }
+  };
